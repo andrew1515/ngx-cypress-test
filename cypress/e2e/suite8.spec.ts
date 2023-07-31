8a3a872700d998d4ac85757a8e48bc74af7f4fc5
andrew1515 <andras15@gmail.com>
2023-07-31 02:46:09 +0200

    advanced interception features

--- cypress/e2e/suite8.spec.ts
+++ cypress/e2e/suite8.spec.ts
@@ -1,6 +1,8 @@
 import { navigation } from "../support/page-objects/navigationPage";
 import { formLayouts } from "../support/page-objects/formLayoutsPage";
 
+// For more detailed informations about interceptions, see: https://docs.cypress.io/api/commands/intercept
+
 describe("intercepts", () => {
   beforeEach(() => {
     cy.openHomePage();
@@ -36,7 +38,7 @@ describe("intercepts", () => {
     });
   });
 
-  it.only("intercepting request from the app with mock data", () => {
+  it("intercepting request from the app with mock data", () => {
     /**
      * Intercepting the request in the app, stopping the sending of the request to the server
      * and providing a mocked response to it from the "users.json".
@@ -78,21 +80,77 @@ describe("intercepts", () => {
     });
   });
 
-  it("intercepting with wildcard url", () => {
+  it("intercepting with wildcard url and route matcher object", () => {
     /**
      * We can use the glob pattern to match URLs. In this case we will match everything that
      * starts with the given URL. (However, we can't have another sub-paths after the /users/, for that
      * we would need to use the "**" pattern - see https://www.malikbrowne.com/blog/a-beginners-guide-glob-patterns/)
+     *
+     * About the router matcher object more here: https://docs.cypress.io/api/commands/intercept#Icon-nameangle-right--routeMatcher-RouteMatcher
      */
-    cy.intercept("GET", "https://jsonplaceholder.typicode.com/users/*", {
-      name: "Andrew Mock",
-      email: "mock@mock.com",
-    });
+    cy.intercept(
+      {
+        method: "GET",
+        // If we wouldn't bother about the hostname, we could just use "pathname: '/users/*'"
+        url: "https://jsonplaceholder.typicode.com/users/*",
+      },
+      {
+        name: "Andrew Mock",
+        email: "mock@mock.com",
+      }
+    );
+
+    /**
+     * Alternative could be using req.reply()
+     * req.reply is useful when we want the mocked response value choose according to some additional logic
+     */
+    // cy.intercept(
+    //   {
+    //     method: "GET",
+    //     url: "https://jsonplaceholder.typicode.com/users/*",
+    //   },
+    //   (req) => {
+    //     req.reply({
+    //       name: "Andrew Mock",
+    //       email: "mock@mock.com",
+    //     });
+    //   }
+    // );
 
     navigation.navigateToFormLayouts();
 
     cy.get(".current-user")
       .find("p")
       .should("contain", "Current user: Andrew Mock | mock@mock.com");
+  });
+
+  it("modifying request/response payload", () => {
+    cy.intercept(
+      "POST",
+      "https://jsonplaceholder.typicode.com/users",
+      (req) => {
+        // Modifying the request body
+        req.body.name = "Modified Andrew";
+
+        // "continue" sends the request to the server and returns the response from the server.
+        // After that we can work with that response, f.e. modify it.
+        // I really can't think of any use-cases for this, because if I would need an exact hardcoded
+        // response in my tests, I would use a fully mocked response without touching the server.
+        // But good to know there is such an option.
+        req.continue((res) => {
+          expect(res.body.name).to.equal("Modified Andrew");
+          res.body.name = "Modified Andrew 2";
+        });
+      }
+    ).as("createNewUser");
+
+    navigation.navigateToFormLayouts();
+    formLayouts.submitInlineForm("Andrew", "andras15@gmail.com");
+
+    cy.wait("@createNewUser").then((interception) => {
+      expect(interception.response.statusCode).to.equal(201);
+      expect(interception.response.body.name).to.equal("Modified Andrew 2");
+      expect(interception.response.body.email).to.equal("andras15@gmail.com");
+    });
   });
 });
